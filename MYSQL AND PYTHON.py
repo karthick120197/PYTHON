ab2abd9a33e00a479445e9f99ec38d2e47e471e9
Karthick Raj <46133222+karthick120197@users.noreply.github.com>
2019-01-23 19:20:34 +0530

    Update MYSQL AND PYTHON.py

--- MYSQL AND PYTHON.py
+++ MYSQL AND PYTHON.py
@@ -17,6 +17,13 @@ df
 from IPython.display import HTML
 import numpy as np
 import base64
+def create_download_link(df, title = "Download CSV file", filename = "data.csv"):  
+    csv = df.to_csv()
+    b64 = base64.b64encode(csv.encode())
+    payload = b64.decode()
+    html = '<a download="{filename}" href="data:text/csv;base64,{payload}" target="_blank">{title}</a>'
+    html = html.format(payload=payload,title=title,filename=filename)
+    return HTML(html)
 create_download_link(df)
 engine = sqlalchemy.create_engine('mysql+pymysql://root:sgk123@localhost:3306/car')
 df = pd.read_sql_table('customer_feedback',engine)
